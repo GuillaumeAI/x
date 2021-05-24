8f479466db50a37709f7d64fa681423a9242237f
JGWill-Orko <jgi@jgwill.com>
2021-05-24 17:57:18 -0400

    works

--- html2markdown.js
+++ html2markdown.js
@@ -5,5 +5,8 @@ var fs = require('fs');
 
 //var html = fs.readFileSync("line.html");
 //var html = fs.readFileSync("line.html");
-var html = "<h1>référence </h1>\n<h2>oeuoeu</h2>";
+var html = fs.readFileSync("buts.html",'utf-8');
+//console.log(html);
+//var html = "<h1>référence </h1>\n<h2>oeuoeu</h2>";
 console.log(html2markdown(html));
+
