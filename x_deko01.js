203a8b5c47ad4c58c3c9002fa66521bed9c514b1
JGWill-Orko <jgi@jgwill.com>
2021-05-27 19:33:52 -0400

    add: deko extraction

---
+++ x_deko01.js
@@ -0,0 +1,56 @@
+
+var deko = require("gixdeko");
+
+var str=`
+----
+ 
+# @v 0 - activité discussion stylistique entre artiste qui me procure de la joie et du plaisir. Une crédibilité avec les artistes participants en relation à ma maitrise en art (rétablissement de l'aspect théorique (conceptuel). / une carrière que j'aime - un horaire de vie et travail équilibré. 
+  
+* 21-05-25 une vue d'une image stylisée avec son original et les méta-données du style
+
+----
+ 
+# @v 2 - Un travail de Prévisualisation de mes œuvres IA adéquat pour le plan de développement de mon style artistique et du matériel d'entraînements 
+ 
+* maîtriser la prévisualisation et ce qui est présenté à ceux avec qui je désire collaborer, ou/et avoir leur soutient 21-06-01 
+* liberté - compétence de a 
+  
+* 21-05-25 Les images sont mise dans des figures intégré à la charte et à Word
+
+----
+ 
+# @v 2.2 - critique Prévisualisation 
+ 
+Maîtriser la construction d'une documentation critique orientée créatif * Maitrise de la collaboration orienté créativem 21-05-31 
+  
+* 21-05-25 prototype ML Arduino démonstratif
+
+----
+ 
+# @v 2.2 - critique Prévisualisation 
+ 
+Maîtriser la construction d'une documentation critique orientée créatif * Maitrise de la collaboration orienté créativem 21-05-31 
+  
+* DONE 21-05-25 intro au IML - un paragraphe
+
+----
+ 
+# @v 2.2 - critique Prévisualisation 
+ 
+Maîtriser la construction d'une documentation critique orientée créatif * Maitrise de la collaboration orienté créativem 21-05-31 
+  
+* DONE 21-05-25 traduire en actions ce qui est annoté' dans l'impression
+
+----
+ 
+# @v Une protéine optimale pour mon alimentation qui respecte la nature et économiquement adéquate 
+ 
+* comblé cérébralement 
+* de l'énergie physique 21-09-15 
+  
+* 21-05-25 retrouvé référence sur où commander
+`;
+
+
+var dk=deko.xtro(str);
+console.log(JSON.stringify(dk));
